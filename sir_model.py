f4cab9b7059be94a868d4f807a1e6ebef9a3068f
Jesse van den Berge <jessevandenberge@gmail.com>
2021-01-18 16:34:56 +0100

    completed the basic version of the model

--- sir_model.py
+++ sir_model.py
@@ -1,25 +1,27 @@
 # This file defines a class for an SIR model
+import numpy as np
+from scipy.integrate import odeint
 
 class SIR_model:
-    def __init__(self, S=10**7, I=100, R=0, t=0,
+    def __init__(self, S0=999, I0=1, R0=0, t0=0, t_max=160,
             vaccinated=0.0, vaccine_effect=0.95, masks=0.0):
         """This method initiates the SIR model. In this model, S is the amount
         of people susceptible to the virus, I the amount of infected and R the
         amount of recovered or dead people."""
         # Adjustable by the user
-        self.S = S
-        self.I = I
-        self.R = R
-        self.t = t
+        self.S0 = S0
+        self.I0 = I0
+        self.R0 = R0
+        self.t = np.linspace(t0, t_max, t_max - t0 + 1)
         self.masks = masks
         self.vaccinated = vaccinated
         self.vaccine_effect = vaccine_effect
         self.growth_rate = 0.2
+        self.recovery_rate = 0.1
+        # self.death_rate = 0.015
 
         # Not adjustable by the user
-        self.recovery_rate = 0.1
-        self.death_rate = 0.015
-        self.N = S + I + R
+        self.N = S0 + I0 + R0
         # 0.5 because most masks have an efficiency of 50%
         # self.mask_coefficient = self.masks * 0.5
 
@@ -28,39 +30,28 @@ class SIR_model:
 
     def run(self):
         """This method runs the simulation of the model."""
-        # This condition should be changed to a case when we would want to stop
-        # the simulation.
-        while self.t < 10:
-            self.step()
+        S, I, R = odeint(self.deriv, [self.S0, self.I0, self.R0], self.t).T
+        return S, I, R
 
-    def step(self):
-        """This method runs a single step in the simulation."""
-        print(self.S, self.I, self.R)
-        self.S += self.dS()
-        self.I += self.dI()
-        self.R += self.dR()
-        self.t += 1
-        # if self.t % 5 == 0:
-        print("Susceptible: ", str(self.S))
-        print("Infected: ", str(self.I))
-        print("Recovered: ", str(self.R))
-        print("Total: ", str((self.S + self.I + self.R)))
+    def deriv(self, y, t):
+        S, I, R = y
+        return self.dS(S, I), self.dI(S, I), self.dR(I)
 
-    def dS(self):
+    def dS(self, S, I):
         """Calculates the difference in the amount of susceptible people for
         a single timestep."""
         # print(-self.growth_rate * self.mask_coefficient() * self.S * self.I)
-        return -self.growth_rate * self.S * self.I / self.N
+        return -self.growth_rate * (S * I / self.N)
 
-    def dI(self):
+    def dI(self, S, I):
         """Calculates the difference in the amount of infected people for
         a single timestep."""
-        return -self.dS() - self.dR()
+        return -self.dS(S, I) - self.dR(I)
 
-    def dR(self):
+    def dR(self, I):
         """Calculates the difference in the amount of recovered/dead people for
         a single timestep."""
-        return self.recovery_rate * self.I
+        return self.recovery_rate * I
 
     def show_results(self):
         """When the simulation is over, this method can be used to visualize
