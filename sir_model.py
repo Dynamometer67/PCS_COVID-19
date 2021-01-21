df03c3c398a0e169dda30f80b6c603b63fcb3392
Jesse van den Berge <jessevandenberge@gmail.com>
2021-01-21 17:19:35 +0100

    yo

--- sir_model.py
+++ sir_model.py
@@ -3,10 +3,8 @@ import numpy as np
 from scipy.integrate import odeint
 
 class SIR_model:
-    def __init__(self, S0=999, I0=1, R0=0, t0=0, t_max=100, growth_rate=0.2,
-            recovery_rate=0.1, vaccinated=0.0, vaccine_efficacy=0.95,
-            mask_coverage=0.0, mask_efficacy=0.0, cm_thresh_low=0.25,
-            cm_thresh_med=0.5, cm_thresh_high=0.75):
+    def __init__(self, S0=999, I0=1, R0=0, t_max=100, growth_rate=0.2,
+                 recovery_rate=0.1, mask_coverage=0.0, mask_efficacy=0.0):
         """This method initiates the SIR model. In this model, S is the amount
         of people susceptible to the virus, I the amount of infected and R the
         amount of recovered or dead people."""
@@ -14,19 +12,12 @@ class SIR_model:
         self.S0 = S0
         self.I0 = I0
         self.R0 = R0
-        self.t0 = t0
         self.t_max = t_max
         self.growth_rate = growth_rate
         self.rec_rate = recovery_rate
-        self.vacc = vaccinated
-        self.vacc_eff = vaccine_efficacy
         self.mask_cov = mask_coverage
         self.mask_eff = mask_efficacy
-        # self.adjustable = [("S0", "S0"), ("I0", "I0"), ("R0", "R0"),
-        #         ("t0", "t0"), ("t_max", "t_max"), ("vaccinated", "vacc"),
-        #         ("vaccine_efficacy", "vacc_eff"), ("mask_coverage", "mask_cov"),
-        #         ("mask_efficacy", "mask_eff")]
-        # self.death_rate = 0.015
+
 
     def mask_factor(self):
         C_mask = self.mask_eff * self.mask_cov
@@ -34,7 +25,7 @@ class SIR_model:
 
     def run(self):
         """This method runs the simulation of the model."""
-        self.t = np.linspace(self.t0, self.t_max, self.t_max - self.t0 + 1)
+        self.t = np.linspace(0, self.t_max, self.t_max + 1)
         self.N = self.S0 + self.I0 + self.R0
         self.beta = self.growth_rate * self.mask_factor()
 
