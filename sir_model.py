5104ecd3f4a38ace5efc4da550f875e9bd9cfcb3
Jesse van den Berge <jessevandenberge@gmail.com>
2021-01-23 16:48:25 +0100

    added stochasticity

--- sir_model.py
+++ sir_model.py
@@ -4,7 +4,8 @@ from scipy.integrate import odeint
 
 class SIR_model:
     def __init__(self, S0=999, I0=10, R0=0, t_max=100, growth_rate=0.2,
-                 recovery_rate=0.1, mask_coverage=0.0, mask_efficacy=0.0):
+                 recovery_rate=0.1, mask_coverage=0.0, mask_efficacy=0.0,
+                 stochastic=1.0):
         """This method initiates the SIR model. In this model, S is the amount
         of people susceptible to the virus, I the amount of infected and R the
         amount of recovered or dead people."""
@@ -17,7 +18,7 @@ class SIR_model:
         self.rec_rate = recovery_rate
         self.mask_cov = mask_coverage
         self.mask_eff = mask_efficacy
-        # self.stochastic = stochastic
+        self.stochastic = stochastic
 
     def mask_factor(self):
         C_mask = self.mask_eff * self.mask_cov
@@ -29,8 +30,14 @@ class SIR_model:
         self.N = self.S0 + self.I0 + self.R0
         self.beta = self.growth_rate * self.mask_factor()
 
+        if self.stochastic:
+            self.euler_maruyama()
+        else:
+            self.run_ODEs()
+
+    def run_ODEs(self):
         self.S, self.I, self.R = \
-                odeint(self.step, [self.S0, self.I0, self.R0], self.t, mxstep=100000).T
+                odeint(self.step, [self.S0, self.I0, self.R0], self.t).T
 
     def step(self, y, t):
         S, I, R = y
@@ -58,41 +65,41 @@ class SIR_model:
 
         return dS, dI, dR
 
-    def step_noise(self, dX):
-        var = abs(dX / 2)
-        noise = np.random.normal(0.0, var)
+    # def step_noise(self, dX):
+    #     var = abs(dX / 2)
+    #     noise = np.random.normal(0.0, var)
+    #
+    #     if (noise > dX and dX >= 0) or (noise < dX and dX <= 0):
+    #         return dX
+    #     return noise
 
-        if (noise > dX and dX >= 0) or (noise < dX and dX <= 0):
-            return dX
-        return noise
-
-    def noise(self, X_diff, mean=0, var_factor=0.5):
-        # Create an array containing noise values with the specified mean and
-        # variance.
-        X_noise = np.random.normal(mean, np.abs(X_diff * var_factor))
-        # Check if X_noise is smaller than X_diff and X_diff is bigger than 0
-        # for each element. This is needed to change all noise values that are
-        # too large with the np.where function. The opposite is also done to
-        # change values that are too small.
-        pos_condition = np.all([X_noise <= X_diff, X_diff >= 0], axis=0)
-        neg_condition = np.all([X_noise >= X_diff, X_diff <= 0], axis=0)
-        # Now check whether either one of the conditions is true for all
-        # elements so that we can use it for np.where. It is used to change all
-        # X_noise values that are bigger than their corresponding value in
-        # X_diff if X_diff is positive and all X_noise values that are smaller
-        # than their corresponding value in X_diff if X_diff is negative to the
-        # values in X_diff. It basically sets boundaries for the values of
-        # X_noise so that the absolute values of X_noise are always smaller
-        # than or equal to the absolute values of X_diff. The reason why it is
-        # complicated is that these statements also make it so that values that
-        # are negative stay negative and values that are positive stay positive.
-        condition = np.any([pos_condition, neg_condition], axis=0)
-        # Set all noise values to be within the boundaries.
-        X_bounded_noise = np.where(condition, X_noise, X_diff)
-
-        # Now make it so that the previous noise values are added to the
-        # current noise value for each noise value.
-        return np.cumsum(X_bounded_noise)
+    # def noise(self, X_diff, mean=0, var_factor=0.5):
+    #     # Create an array containing noise values with the specified mean and
+    #     # variance.
+    #     X_noise = np.random.normal(mean, np.abs(X_diff * var_factor))
+    #     # Check if X_noise is smaller than X_diff and X_diff is bigger than 0
+    #     # for each element. This is needed to change all noise values that are
+    #     # too large with the np.where function. The opposite is also done to
+    #     # change values that are too small.
+    #     pos_condition = np.all([X_noise <= X_diff, X_diff >= 0], axis=0)
+    #     neg_condition = np.all([X_noise >= X_diff, X_diff <= 0], axis=0)
+    #     # Now check whether either one of the conditions is true for all
+    #     # elements so that we can use it for np.where. It is used to change all
+    #     # X_noise values that are bigger than their corresponding value in
+    #     # X_diff if X_diff is positive and all X_noise values that are smaller
+    #     # than their corresponding value in X_diff if X_diff is negative to the
+    #     # values in X_diff. It basically sets boundaries for the values of
+    #     # X_noise so that the absolute values of X_noise are always smaller
+    #     # than or equal to the absolute values of X_diff. The reason why it is
+    #     # complicated is that these statements also make it so that values that
+    #     # are negative stay negative and values that are positive stay positive.
+    #     condition = np.any([pos_condition, neg_condition], axis=0)
+    #     # Set all noise values to be within the boundaries.
+    #     X_bounded_noise = np.where(condition, X_noise, X_diff)
+    #
+    #     # Now make it so that the previous noise values are added to the
+    #     # current noise value for each noise value.
+    #     return np.cumsum(X_bounded_noise)
 
     def dS(self, S, I):
         """Calculates the difference in the amount of susceptible people for
@@ -109,25 +116,50 @@ class SIR_model:
         a single timestep."""
         return self.rec_rate * I
 
-    def add_stochasticity(self):
-        S_diff = np.diff(self.S)
-        R_diff = np.diff(self.R)
+    # def add_stochasticity(self):
+    #     S_diff = np.diff(self.S)
+    #     R_diff = np.diff(self.R)
+    #
+    #     S_noise = self.noise(S_diff)
+    #     R_noise = self.noise(R_diff)
+    #
+    #     self.S[1:] += S_noise
+    #     self.I[1:] -= S_noise + R_noise
+    #     self.R[1:] += R_noise
+    #
+    #     print(self.S + self.I + self.R)
 
-        S_noise = self.noise(S_diff)
-        R_noise = self.noise(R_diff)
-
-        self.S[1:] += S_noise
-        self.I[1:] -= S_noise + R_noise
-        self.R[1:] += R_noise
-
-        print(self.S + self.I + self.R)
+    def dW(self, dX, mu, sigma_factor):
+        dW = np.random.normal(mu, abs(sigma_factor * dX))
+        if (dX >= 0 and dW > dX) or (dX <= 0 and dW < dX):
+            return dX
+        return dW
 
     def euler_maruyama(self):
         """
         https://www.math.kit.edu/ianm3/lehre/nummathfin2012w/media/euler_maruyama.pdf
         https://en.wikipedia.org/wiki/Euler%E2%80%93Maruyama_method
         """
-        pass
+        self.S = np.zeros(self.t.shape)
+        self.I = np.zeros(self.t.shape)
+        self.R = np.zeros(self.t.shape)
+        self.S[0] = self.S0
+        self.I[0] = self.I0
+        self.R[0] = self.R0
+        mu = 0.0
+        sigma_factor = 0.5
+
+        for i in range(1, len(self.t)):
+            dS = self.dS(self.S[i-1], self.I[i-1])
+            dI = self.dI(self.S[i-1], self.I[i-1])
+            dR = self.dR(self.I[i-1])
+            S_noise = self.dW(dS, mu, sigma_factor)
+            R_noise = self.dW(dR, mu, sigma_factor)
+
+            self.S[i] = self.S[i-1] + dS + S_noise
+            self.I[i] = self.I[i-1] + dI - S_noise - R_noise
+            self.R[i] = self.R[i-1] + dR + R_noise
+
 
     def show_results(self):
         """When the simulation is over, this method can be used to visualize
